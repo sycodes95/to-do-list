586c29d3dc41beb367137c8bdc6ce6ef09a80f24
sycodes95 <sycodes95@gmail.com>
2022-11-06 19:07:24 -0800

    3rd Version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 let myProjects = [];
 
+class Project {
+    constructor(title, index, tasks){
+        this.title = title
+        this.index = index
+        this.tasks = []
+    }
+
+}
+
+class Task {
+    constructor(title, description, priority, dueDate){
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+}
+
 (function projectButton (){
     const projectFormContainer = document.querySelector('.projectFormContainer');
     const projectButton = document.querySelector('.projectButton');
@@ -42,62 +60,92 @@ function taskButton (){
 function submitTask (i){
     const submitTask = document.getElementById('submitTask');
 
-    
+    const taskContainer = document.querySelector('.taskContainer')
+    taskContainer.innerHTML = '';
+
+    const taskTitle = document.getElementById('taskTitle');
+    const taskDescription = document.getElementById('taskDescription');
+    const taskPriority = document.getElementById('taskPriority');
+
     submitTask.addEventListener('click', ()=>{
+
+        
+
+
         let title = document.getElementById('taskTitle').value;
         let description = document.getElementById('taskDescription').value;
         let priority = document.getElementById('taskPriority').value
 
         let taskObj = new Task (title, description, priority)
-        myProjects.at(i).tasks = taskObj;
+        myProjects.at(i).tasks.push(taskObj);
+        
+
+        
+
+        
+
+        
+        
         console.log(myProjects)
 
-        
     })
 }
         
-        
-        
-        
-        
-
-
-        
-        
-
 function projectClick (){
     const projectContainer = document.querySelector('.projectContainer');
-    let contentHeader = document.querySelector('.contentHeader')
+    let contentHeader = document.querySelector('.contentHeader');
+    const taskContainer = document.querySelector('.taskContainer')
     Array.from(projectContainer.children).forEach((project, i) =>{
         
+        
         project.addEventListener('click',()=>{
+            taskContainer.innerHTML = '';
             contentHeader.textContent = `${myProjects[i].title}`;
-            console.log(project)
+            
+            
+
+            
             console.log(myProjects)
+            console.log(i)
+            
+            
         })
 
-        taskButton()
+        displayTasks(i)
         submitTask(i)
+        taskButton()
+        
     })
 }
 
-class Project {
-    constructor(title, index, tasks){
-        this.title = title
-        this.index = index
-        this.tasks = []
-    }
+function displayTasks (i){
+
+    
+    const taskContainer = document.querySelector('.taskContainer');
+    taskContainer.innerHTML = '';
+    myProjects.at(i).tasks.forEach((e) =>{
+        let task = document.createElement('div');
+        task.textContent = e.title;
+        taskContainer.appendChild(task)
+
+
+    })
 
 }
 
-class Task {
-    constructor(title, description, priority, dueDate){
-        this.title = title;
-        this.description = description;
-        this.priority = priority;
-        this.dueDate = dueDate;
-    }
-}
+
+        
+        
+        
+        
+        
+
+
+        
+        
+
+
+
             
 
 
